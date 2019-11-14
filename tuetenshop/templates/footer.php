ddb4537ac49bd4e6cb381bc3e62334c7d1e2fc2e
Robin Avci <50947565+RobinWilliamAvci@users.noreply.github.com>
2019-11-14 14:13:48 +0100

    Add files via upload

---
+++ tuetenshop/templates/footer.php
@@ -0,0 +1,43 @@
+        <footer>
+            <div class="footer-spalten">
+
+                <div class="footer-spalte">
+                    <ul>    
+                        <li><a href="datenschutz.html">About US</a></li>
+                        <li><a href="imprint.html">Impressum</a></li>
+                        <li><p>Erfahre alles </p></li>
+                    </ul>
+                </div>
+
+                <div class="footer-spalte">
+                    <ul>    
+                        <li><a href="datenschutz.html">FAQ</a></li>
+                        <li><a href="imprint.html">AGB</a></li>
+                        <li><a href="imprint.html">Datenschutz</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-spalte">
+                    <ul>    
+                        <li><p>Kontaktiere uns</p></li>
+                        <li><p>030/12345678</p></li>
+                        <li><p>Mail: abc@tuetenshop.nice</p></li>
+                    </ul>
+                </div>
+
+                <div class="footer-spalte" id="lange-footer-spalte">
+                    <ul>    
+                        <li><p>Melde dich hier für den Newsletter an!</p></li>
+                        <li><input type="e-mail" id="newsletteranmeldefeld" placeholder="E-Mail-Adresse hier eingeben..."></li>
+                        <li>
+                            <img src="pictures/payments/paypal.png" alt="paypal" id="payments">
+                            <img src="pictures/payments/amazon.png" alt="amazon" id="payments">
+                            <img src="pictures/payments/visa.png" alt="visa" id="payments">
+                        </li>
+                    </ul>
+                </div>    
+
+            </div>            
+        </footer>
+    </body>
+</html>
---
+++ tuetenshop/templates/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Tütenshop!</title>
+        <link rel="stylesheet" type="text/css" href="newstyle.css"/>
+    </head>
+    
+    <body>
+        <header>
+
+            <div class="Logo">
+                <img src="pictures/logo.png" alt="Logo des Unternehmens">
+            </div>
+
+            <div class="HalloUser">
+                    <p>Hallo, $User! <a href="login.php">einloggen</a></p>     
+            </div> 
+
+            <nav>
+                <ul>
+                        <li class="highlight-button"><a href="startseite.php">Create your Bags</a></li>
+                        <li><a href="login.php">Account</a></li>
+                        <li><a href="kunden.php">ABG</a></li>
+                        <li><a href="geheim.php">Datenschutz</a></li>
+                        <li><a href="">PH</a></li>
+                </ul>
+            </nav>
+
+            <div class="abstandhalter">
+                <h6>Fragen zum Produkt? Ruf uns einfach an! 030/12345678 - Mo bis Fr von 09 bis 16 Uhr</h6>
+            </div>
+
+        </header>
+
