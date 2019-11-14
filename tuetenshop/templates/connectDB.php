5b4761dd0d9847a2feb4b9ef64a9e38ca1e5a3c1
Robin <Git-Avci@outlook.com>
2019-11-14 22:17:28 +0100

    robin

--- tuetenshop/templates/connectDB.php
+++ tuetenshop/templates/connectDB.php
@@ -2,7 +2,7 @@
 
     /*Verbindung mit der Datenbank*/
 
-    $db_link = mysqli_connect('localhost', 'root', '', 'webshop');
+    $db_link = mysqli_connect('localhost', 'root', '#Adler007', 'webshop');
 
     if(!$db_link)
     {
